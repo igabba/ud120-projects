85ba1933f393da1b69455e3035ec0869e40de733
igabba <ignaciogabbarini@gmail.com>
2017-05-30 10:34:34 -0300

    svm, naives bayes, decision tree

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,7 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
@@ -24,6 +25,16 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+
+
+clf = SVC(kernel='rbf', cache_size=400, C=10000)
+#features_train = features_train[:len(features_train)/100]
+#labels_train = labels_train[:len(labels_train)/100]
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print accuracy_score(pred, labels_test)
+print sum(1 for value in pred if value == 1)
+print sum(1 for value in pred if value == 0)
 
 #########################################################
 
