154641e09af3e06ae46cdb871d572a0feac643df
igabbarini <ignacio.gabbarini@avantrip.com>
2017-06-13 15:08:09 -0300

    text

--- text_learning/vectorize_text.py
+++ text_learning/vectorize_text.py
@@ -5,8 +5,9 @@ import pickle
 import re
 import sys
 
-sys.path.append( "../tools/" )
+sys.path.append("../tools/")
 from parse_out_email_text import parseOutText
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 """
     Starter code to process the emails from Sara and Chris to extract
@@ -22,54 +23,52 @@ from parse_out_email_text import parseOutText
     The data is stored in lists and packed away in pickle files at the end.
 """
 
-
-from_sara  = open("from_sara.txt", "r")
+from_sara = open("from_sara.txt", "r")
 from_chris = open("from_chris.txt", "r")
 
 from_data = []
 word_data = []
 
-### temp_counter is a way to speed up the development--there are
-### thousands of emails from Sara and Chris, so running over all of them
-### can take a long time
-### temp_counter helps you only look at the first 200 emails in the list so you
-### can iterate your modifications quicker
-temp_counter = 0
-
-
+# temp_counter is a way to speed up the development--there are
+# thousands of emails from Sara and Chris, so running over all of them
+# can take a long time
+# temp_counter helps you only look at the first 200 emails in the list so you
+# can iterate your modifications quicker
+sw = ["sara", "shackleton", "chris", "germani"]
 for name, from_person in [("sara", from_sara), ("chris", from_chris)]:
     for path in from_person:
-        ### only look at first 200 emails when developing
-        ### once everything is working, remove this line to run over full dataset
-        temp_counter += 1
-        if temp_counter < 200:
-            path = os.path.join('..', path[:-1])
-            print path
-            email = open(path, "r")
+        # only look at first 200 emails when developing
+        # once everything is working, remove this line to run over full dataset
+        path = os.path.join('..', path[:-1])
+        print path
+        email = open(path, "r")
 
-            ### use parseOutText to extract the text from the opened email
+        text = str(parseOutText(email))
+        for rm in sw:
+            if(rm in text):
+                text = text.replace(rm,"")
 
-            ### use str.replace() to remove any instances of the words
-            ### ["sara", "shackleton", "chris", "germani"]
+        word_data.append(text)
 
-            ### append the text to word_data
+        if name == "Sara":
+            from_data.append(0)
+        if name == "Chris":
+            from_data.append(1)
 
-            ### append a 0 to from_data if email is from Sara, and 1 if email is from Chris
-
-
-            email.close()
+        email.close()
 
 print "emails processed"
 from_sara.close()
 from_chris.close()
+print word_data[152]
 
-pickle.dump( word_data, open("your_word_data.pkl", "w") )
-pickle.dump( from_data, open("your_email_authors.pkl", "w") )
+pickle.dump(word_data, open("your_word_data.pkl", "w"))
+pickle.dump(from_data, open("your_email_authors.pkl", "w"))
 
+# in Part 4, do TfIdf vectorization here
+vectorizer = TfidfVectorizer(stop_words='english')
+vec_fit=vectorizer.fit_transform(word_data)
 
-
-
-
-### in Part 4, do TfIdf vectorization here
-
-
+print len(vectorizer.get_feature_names())
+vec_words = vectorizer.get_feature_names()
+print vec_words[34597]
